4d8d24324aa44c3d51290fda76a0f385be39a933
Shreyas <shreyas0906@gmail.com>
2016-03-03 15:46:38 -0500

    changed in bounding box for face

--- face_test.py
+++ face_test.py
@@ -9,13 +9,10 @@ while True:
     ret, img = cap.read()
     img  = cv2.resize(img,(640,480))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
     # find the sungalasses within the face
-
-
-
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,100,100),1)
         print "w", w, "h", h , "x", x , "y", y
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
@@ -24,9 +21,9 @@ while True:
         #eyes = eye_cascade.detectMultiScale(roi_gray)
         sunglasses = sunglasses_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in sunglasses:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(100,255,100),1)
         cv2.imshow('area',area)
-    cv2.imshow('img',img)
+        cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
