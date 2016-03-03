b225c7afee803b1373310b793bf758767a5e7df9
Shreyas <shreyas0906@gmail.com>
2016-03-03 15:39:03 -0500

    sunglasses detection with image, with displaying only  face areas

--- face_test.py
+++ face_test.py
@@ -16,14 +16,16 @@ while True:
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        print w,h
+        print "w", w, "h", h , "x", x , "y", y
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
+        area = img[y:y+h+15,x:x+w+15]
+
         #eyes = eye_cascade.detectMultiScale(roi_gray)
         sunglasses = sunglasses_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in sunglasses:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
+        cv2.imshow('area',area)
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
