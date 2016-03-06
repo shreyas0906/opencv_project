69ffe34db7139c39d4bdd6898e472c70984b2977
Shreyas <shreyas0906@gmail.com>
2016-03-06 16:02:50 -0500

    changed in bounding box for face

--- testing.py
+++ testing.py
@@ -4,14 +4,13 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 sunglasses_cascade = cv2.CascadeClassifier('cascade_sunglasses.xml')
-
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
+    gray = cv2.resize(gray,(640,480))
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
--- testing_face.py
+++ testing_face.py
@@ -8,22 +8,17 @@ while True:
     img  = cv2.resize(img,(640,480))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray,1.3,5)
-    # find the sungalasses within the face
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,100,100),1)
-        print "w", w, "h", h , "x", x , "y", y
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,150,150),1)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        area = img[y:y+h+15,x:x+w+15]
-        sunglasses = sunglasses_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in sunglasses:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(100,255,100),1)
-        cv2.imshow('area',area)
-        cv2.imshow('img',img)
+        cv2.imshow('Region_color',roi_color)
+        cv2.imhsow('Region_gray',roi_gray)
+         for (ex,ey,ew,eh) in faces:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(190,160,110),1)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
-
-cap.release()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
